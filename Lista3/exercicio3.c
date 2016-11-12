1a743d05d734e6844204352804342d4ece7ee89d
Reinaldo A. J <juni127@live.com>
2016-11-12 09:16:17 -0200

    Ideia do Jhon implementada

--- Lista3/exercicio3.c
+++ Lista3/exercicio3.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//Função original
 void mistura(int *a, int as, int *b, int bs, int *c){
   int x, ta = 0, tb = 0;
   for(x = 0; x < as+bs; x++){
@@ -12,6 +13,30 @@ void mistura(int *a, int as, int *b, int bs, int *c){
       ta++;
     }
   }
+}
+
+//Ideia do jhon
+void misturaJ(int *a, int as, int *b, int bs, int *c){
+  //A variavel topoC serve para marcar onde paramos no vetor C, a topoB para marcar no vetor B
+  int x, y, topoC, topoB = 0;
+  //Coloca a em c
+  for(x = 0; x < as; x++){
+    c[x] = a[x];
+  }
+  topoC = as;
+  for(x = 0; topoC < as+bs; x++){
+    //Se o valor de b teria que vir nessa posição em c
+    if(b[topoB] < c[x]){
+      //Adianta todos os valores de c na frente de x em uma casa
+      for(y = topoC+1; y > x; y--)c[y] = c[y-1];
+      //Coloca o valor de b em c
+      c[x] = b[topoB];
+      topoB++;
+      topoC++;
+    }
+    //Se não não fazemos nada
+  }
+
 }
 
 int main(){
@@ -39,7 +64,7 @@ int main(){
   system("cls");
 
   int vetC[sa+sb];
-  mistura(vetA, sa, vetB, sb, vetC);
+  misturaJ(vetA, sa, vetB, sb, vetC);
 
   printf("VET-C = [%i", vetC[0]);
   for(x = 1; x < sa+sb; x++)printf(", %i", vetC[x]);
