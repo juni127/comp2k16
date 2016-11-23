f65e0f3a79b555467ed796e8953b229f2f0d1ee3
Reinaldo <juni127@live.com>
2016-11-23 17:29:15 -0200

    Working...

--- Lista4/exercicio3.c
+++ Lista4/exercicio3.c
@@ -1,11 +1,22 @@
-//Escrito com Atom
+//Escrito por Sandro
 #include<stdio.h>
 #include<stdlib.h>
 
+//Como eu fiz
 int maiorElemento(int *vet, int t, int max){
   if(t == 0)return max;
   if(*vet > max)max = *vet;
   return maiorElemento(vet+1, t-1, max);
+}
+
+
+//Como a Ines espera que seja feito
+//Ta quebrado :[
+int maximo(int *vet, int t){
+  if(t == 1)return *vet;
+  int max = maximo(vet, t-1);
+  if(*(vet+t) > max)return *(vet+t);
+  else return max;
 }
 
 int main(){
@@ -19,7 +30,7 @@ int main(){
     scanf("%i", &vet[x]);
   }
 
-  printf("O maior elemento foi: %i\n", maiorElemento(vet, n, 0));
+  printf("O maior elemento foi: %i\n", maximo(vet, n));
 
   system("pause");
   return 0;
