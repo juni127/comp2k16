76a8726ceb10f118cafc3c07f8fd0f32c2fc65c0
Reinaldo Apolinario Junior <reinaldo@r09l01.COMPUTACAO>
2017-07-12 13:34:25 -0300

    Multilingua

--- ED/Trabalho/main.c
+++ ED/Trabalho/main.c
@@ -4,6 +4,33 @@
 
 //1->sim->direita 0->não->esquerda
 
+int trataResposta(char e){
+	if(e < 50)e -= 48;
+	//Sim
+	else if (e == 83)e = 1;
+	else if (e == 115)e = 1;
+	//Oui
+	else if (e == 79)e = 1;
+	else if (e == 111)e = 1;
+	//Yes
+	else if (e == 89)e = 1;
+	else if (e == 121)e = 1;
+	//Hai
+	else if (e == 72)e = 1;
+	else if (e == 104)e = 1;
+	//Ja
+	else if (e == 74)e = 1;
+	else if (e == 106)e = 1;
+	//Não - Non - No - Nein
+	else if (e == 110)e = 0;
+	else if (e == 78)e = 0;
+	//Iie
+	else if (e == 73)e = 0;
+	else if (e == 105)e = 0;
+	else e = 0;
+	return e;
+}
+
 int main(int argc, char const *argv[]) {
 
   FILE *save = fopen("save.txt", "r");
@@ -12,31 +39,31 @@ int main(int argc, char const *argv[]) {
   fclose(save);
   Tree *raiz = dados;
   int e;
+  char in[50];
 
   //Loop principal
   do{
     //Limpa tela -> Windows only
     system("cls");
-    puts("Digite 1 pra sim e 0 pra nao.");
+    //puts("Digite 1 pra sim e 0 pra nao.");
     //Raiz serve para armazenar o primeiro nó, já que eu uso dados para caminhar pela arvore
     dados = raiz;
     //Loop de perguntas para descobrir o nome do aluno
-    while(1){ //while(1) executa o codigo indeterminadamente
+    while(dados->e != NULL){ //while(1) executa o codigo indeterminadamente
       //Imprime o conteudo do nó (Como ele tem raiz ele é uma pergunta).
       printf("%s\n", dados->conteudo);
       //Lê a resposta do usuario
-      scanf("%i", &e);
+      scanf("%s", in);
+	  e = trataResposta(in[0]);
       //Se e = 0 -> dados->e é retornado se não dados->d é retornado
       dados = (e)?dados->d:dados->e;
-      //Se o nó não possui raiz então ele é uma resposta e o loop tem que ser quebrado
-      if(dados->e == NULL)
-        break;
     }
 
     //Imprime o chute do programa
     printf("O nome do aluno eh %s?\n", dados->conteudo);
     //Lê a resposta do usuario
-    scanf("%i", &e);
+    scanf("%s", in);
+	e = trataResposta(in[0]);
     //Se e = 0
     if(!e){
       //Se prepara para receber os novos dados
@@ -73,7 +100,8 @@ int main(int argc, char const *argv[]) {
       puts("Sou um otimo advinho!");
     }
     puts("Deseja jogar novamente?");
-    scanf("%i", &e);
+    scanf("%s", in);
+	e = trataResposta(in[0]);
   }while(e != 0);
 
   //Salva o arquivo
