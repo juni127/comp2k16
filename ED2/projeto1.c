4e95e5230fa3046b584be44a1ad7c6b4d28945ea
Reinaldo Apolinario Junior <reinaldo@r09l01.COMPUTACAO>
2017-08-15 21:43:19 -0300

    Trampo 1 ED 2 v0.3

--- ED2/projeto1.c
+++ ED2/projeto1.c
@@ -1,83 +1,154 @@
-#include<stdlib.h>
+
 #include<stdio.h>
-#include <conio.h>
+#include<time.h>
+
+#include"util.h"
 
 #define MAX_READY_SIZE 5
 #define MAX_QUANTUM_SIZE 20
 
-typedef struct PCB{
-    short id;
-    //Estado do processo 8bits -> vamos usar só 2 mas é isso ae
-    char PS;
-    //Contador de programa
-    char PC;
-    //Tempo utilizado de CPU
-    int T;
-
-    //Proximo PCB
-    struct PCB *p;
-}PCB;
-
-typedef struct PROC{
-    //SHORT 16bits -> 65.536 proc no maximo
-    short id;
-    //Instruções
-    char text[256];
-
-    //Proximo PROC
-    struct PROC *p;
-    //PCB associado
-    PCB pcb;
-}PROC;
-
 int main(int argc, char *args[]){
-    //abrir as paradinha
+    //Variavel para todos os usos
+    int x;
+
+    //Preparar a "aleatoriade"
+    srand(time(NULL));
+
+    //Gerar ids validos
+    for(x = 0; x < 65536; x++)
+        idDisponiveis[x] = x;
 
     //Filas
-    PROC *jobs, *ready, *device;
-    PCB *PCBT;
+    PROC *jobs = NULL, *ready = NULL, *device = NULL;
+    PCB *PCBT = NULL, *pcb = NULL;
+
 
     //Loop principal
-    while (NULL) {
+    for (;;) {
+        /*
+            Flags
+            0x01 -> Interrupção
+            0x02 -> Encerrado
+        */
+        char f = 0;
 
-        //RODAR
-        char quantum = 0;
-        //Pegar mover PCBT para o PCB relacionado ao processo rodando agora
-        PCBT = getPCB(PCBT, ready->id);
-        //Executar as instruções do processo a menos que, o seu quantum tenha se esgotado ou suas instruções tenham acabado
-        while (quantum < 20 && ready->text[PCBT->PC] != 0xFF){
-            //Aumenta a posição do contador de programa
-            PCBT->PC++;
-            //Aumenta o tempo de execução do programa
-            PCBT->T++;
-            //Aumenta o quantum do programa
-            quantum++;
-            //Copiar o PCBT de volta pro PCB no processo
-            ready->pcb = PCBT;
-            ready->pcb.p = NULL;
-            //Interrupção
-            if(ready->text[PCBT->PC] == 0x53){
-                //Adicionar o processo atual a fila de dispositivos
-                queueAdd(device, queueRemove(ready));
-                //Remover o PCB associado da PCBT
-                purgePCB(PCBT);
-                break;
-            }
+        puts("1");
+
+        //ALEATORIEDADES
+        //Adicionar um processo
+        if(rand()%60000 < 60000-queueSize(jobs)){
+            PROC *novo = (PROC*)malloc(sizeof(PROC));
+            //Criar instruções
+            unsigned char pointer = 0;
+            do{
+                unsigned char teste = rand()%256;
+                if(teste < 250)
+                    novo->text[pointer] = teste;
+                else if(teste < 253)
+                    novo->text[pointer] = 0xFD;
+                else
+                    novo->text[pointer] = 0xFF;
+                pointer++;
+            }while(novo->text[pointer-1] != 0xFF && pointer < 255);
+            if(pointer == 255)
+                novo->text[pointer] = 0xFF;
+            //Atribuir id valido
+            novo->id = getId();
+            //Prencher PCB
+            novo->pcb.id = novo->id;
+            novo->pcb.PS = 0;
+            novo->pcb.PC = 0;
+            novo->pcb.T = 0;
+            novo->pcb.p = NULL;
+            //O novo programa ainda não tem próximo
+            novo->p = NULL;
+
+            //Adicionar na fila de trabalhos
+            jobs = queueAdd(jobs, novo);
         }
 
-        //Verificar se o processo foi encerrado
-        if(ready->text[PCBT->PC] == 0xFF || ready->text[PCBT->PC + 1] == 0xFF){
-
-        }
+        puts("2");
 
         //A fila de programas prontos não está cheia
         if(queueSize(ready) < MAX_READY_SIZE){
             //Adicionar trabalho da fila de espera a fila de programas prontos
-            PROC *aux = queueRemove(jobs);
-            addPCBT(PCBT, aux->pcb);
-            queueAdd(ready, aux);
+            PROC *aux = jobs;
+            aux->p = NULL;
+            jobs = queueRemove(jobs);
+            pcb = addPCB(pcb, aux->pcb);
+            if(ready != NULL)
+                ready = queueAdd(ready, aux);
+            else{
+                ready = aux;
+                ready->p = aux;
+            }
         }
 
+        puts("3");
+
+
+        char quantum = 0;
+        if(queueSize(ready) > 0){
+            //RODAR
+            //Ir para o próximo ready
+            ready = ready->p;
+            //Pegar mover PCBT para o PCB relacionado ao processo rodando agora
+            PCBT = getPCB(pcb, ready->id);
+            printf("%p\n", PCBT);
+            //Executar as instruções do processo a menos que, o seu quantum tenha se esgotado ou suas instruções tenham acabado
+            while (quantum < 20 && ready->text[PCBT->PC] != 0xFF){
+                puts("A");
+                //Aumenta a posição do contador de programa
+                PCBT->PC++;
+                puts("B");
+                //Aumenta o tempo de execução do programa
+                PCBT->T++;
+                puts("C");
+                //Aumenta o quantum do programa
+                quantum++;
+                puts("D");
+                //Copiar o PCBT de volta pro PCB no processo
+                ready->pcb = *PCBT;
+                ready->pcb.p = NULL;
+                puts("E");
+                //Interrupção
+                if(ready->text[PCBT->PC] == 0xFD){
+                    //Setar flag
+                    f |= 0x01;
+                    //Adicionar o processo atual a fila de dispositivos
+                    PROC *aux = ready;
+                    ready = queueRemove(ready);
+                    device = queueAdd(device, aux);
+                    //Remover o PCB associado da PCBT
+                    pcb = purgePCB(PCBT, pcb);
+                    break;
+                }
+                puts("D");
+            }
+
+            //Verificar se o processo foi encerrado (Caso ele não tenha ido pra fila de dispositivos)
+            if(!(f & 0x01) && (ready->text[PCBT->PC] == 0xFF || ready->text[PCBT->PC + 1] == 0xFF)){
+                //Setar flag
+                f |= 0x02;
+                //Eliminar processo
+                ready = queuePurge(ready);
+            }
+
+        }
+
+        puts("4");
+
         //Printar os dados da rodada
+        puts("Dados:");
+        printf("Quantum utilizado: %d\n", quantum);
+        printf("Processos na fila de jobs: %d\n", queueSize(jobs));
+        printf("Processos na fila de prontos: %d\n", queueSize(ready));
+        printf("Processos na fila de dispositivos: %d\n", queueSize(device));
+        if(f & 0x01)
+            puts("Processo interrompido.");
+        if(f & 0x02)
+            puts("Processo concluido.");
+
+        system("pause");
     }
 }
---
+++ ED2/util.h
@@ -0,0 +1,159 @@
+#include<stdlib.h>
+
+unsigned short idDisponiveis[65536];
+
+typedef struct PCB{
+    unsigned short id;
+    //Estado do processo 8bits -> vamos usar só 2 mas é isso ae
+    unsigned char PS;
+    //Contador de programa
+    unsigned char PC;
+    //Tempo utilizado de CPU
+    int T;
+
+    //Proximo PCB
+    struct PCB *p;
+}PCB;
+
+typedef struct PROC{
+    //SHORT 16bits -> 65.536 proc no maximo
+    unsigned short id;
+    //Instruções
+    unsigned char text[256];
+
+    //Proximo PROC
+    struct PROC *p;
+    //PCB associado
+    PCB pcb;
+}PROC;
+
+PROC *queueRemove(PROC *target){
+    //Não existe o que remover
+    if(target == NULL)
+        return target;
+    //Cria variavel auxiliar
+    PROC *aux = target;
+    //Lidar com lista circular
+    while (aux->p != NULL) {
+        if(aux->p == target){
+            aux->p = target->p;
+            break;
+        }
+        aux = aux->p;
+    }
+    //Desligar nó da fila
+    aux = target->p;
+    target->p = NULL;
+    return aux;
+}
+
+PROC *queueAdd(PROC *queue, PROC *target){
+    //Queue vazio
+    if(queue == NULL)
+        return target;
+    //Criar variavel auxiliar
+    PROC *aux = queue;
+    //Ir até o ultimo elemento da fila
+    while (aux->p != NULL) {
+        if(aux->p == queue){
+            break;
+        }
+        aux = aux->p;
+    }
+    //Adicionar o novo elemento
+    target->p = aux->p;
+    aux->p = target;
+    return queue;
+}
+
+PROC *queuePurge(PROC *target){
+    if(target == NULL)
+        return NULL;
+    if(target == target->p)
+        return NULL;
+    PROC *aux = target;
+    while(aux->p != NULL){
+        if(aux->p == target){
+            aux->p = target->p;
+            break;
+        }
+        aux = aux->p;
+    }
+    aux = target->p;
+    free(target);
+    return aux;
+}
+
+int queueSize(PROC *queue){
+    //Fila vazia
+    if(queue == NULL)
+        return 0;
+    int result = 1;
+    PROC *aux = queue;
+    while (aux->p != NULL) {
+        result++;
+        if(aux->p == queue){
+            break;
+        }
+        aux = aux->p;
+    }
+    return result;
+}
+
+PCB *getPCB(PCB *pcb, unsigned short id){
+    if(pcb == NULL)
+        return NULL;
+    if(pcb->id == id)
+        return pcb;
+    while(pcb->p != NULL){
+        if(pcb->id == id)
+            return pcb;
+        pcb = pcb->p;
+    }
+    return NULL;
+}
+
+PCB *purgePCB(PCB *PCBT, PCB *pcb){
+    if(PCBT == pcb){
+        pcb = PCBT->p;
+        free(PCBT);
+        return pcb;
+    }
+    PCB *aux = pcb;
+    while(aux->p != PCBT)
+        aux = aux->p;
+    aux->p = PCBT->p;
+    free(PCBT);
+    return pcb;
+}
+
+PCB *addPCB(PCB *pcb, PCB PCBT){
+    PCB *novo = (PCB*)malloc(sizeof(PCB)), *aux = pcb;
+    *novo = PCBT;
+    novo->p = NULL;
+    if(pcb == NULL)
+        return novo;
+    while(aux->p != NULL)
+        aux = aux->p;
+    aux->p = novo;
+    return pcb;
+}
+
+unsigned short getId(){
+    unsigned short result = idDisponiveis[0], c, i;
+    for(c = 0; idDisponiveis[c] != 65535; c++){
+        idDisponiveis[c] = idDisponiveis[c + 1];
+    }
+    return result;
+}
+
+void returnId(unsigned short id){
+    unsigned short a = 0, c, i;
+    for(c = 0; idDisponiveis[c] != 65535; c++)
+        if(idDisponiveis[c] < id)
+            a++;
+    a++;
+    for(i = c; i > a; i--)
+        idDisponiveis[i] = idDisponiveis[i - 1];
+    idDisponiveis[a] = id;
+}
