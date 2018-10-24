b7f7990fc46d680978f170ab02c00bda7f6bb11a
Reinaldo A. Junior <juni127@live.com>
2018-10-24 19:45:54 -0300

    aula 2 cg completa

--- CG/aula2.c
+++ CG/aula2.c
@@ -6,11 +6,8 @@
 //#include <stdio.h>
 //#include <GL/freeglut.h>
 
-//Declaração de Variáveis Globais
-int projecao=0; //Variável Lógica para Definir o Tipo de Projeção (Perspectiva ou Ortogonal)
-int posx=0, posy=10, posz=20; //Variáveis que definem a posição da câmera
-int oy=0,ox=0,oz=0;         //Variáveis que definem para onde a câmera olha
-int lx=0, ly=1,  lz=0;         //Variáveis que definem qual eixo estará na vertical do monitor.
+//DeclaraÃ§Ã£o de variaveis
+int g1 = 45, g2 = 45, g0 = 45, s = 0;
 
 //Protótipos das Funções
 void Inicializa();
@@ -39,17 +36,20 @@ void Display()
    glLoadIdentity();//"Limpa" ou "transforma" a matriz em identidade, reduzindo possíveis erros.
 
 
-   //gluPerspective(45,1,1,150);
-	glOrtho(-8, 8, -8, 8, -150, 150);
+   	gluPerspective(45,1,1,150);
+	//glOrtho(-8, 8, -8, 8, -150, 150);
 
    glMatrixMode(GL_MODELVIEW);
    glLoadIdentity();
-   gluLookAt(0, 0, 10, 0, 0, 0, lx, ly, lz); //Define a pos da câmera, para onde olha e qual eixo está na vertical.
+   gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0); //Define a pos da câmera, para onde olha e qual eixo está na vertical.
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
    glColor3ub(255, 255, 255);
 
 	//Codigo
+
+	glTranslatef(0, -2, 0);
+	glRotatef(g0, 0, 1, 0);
 
 	glutWireCube(1);
 
@@ -58,9 +58,11 @@ void Display()
 	glutWireSphere(0.5, 10, 10);
 	glPopMatrix();
 
+	glTranslatef(0, 0.75, 0);
+
 	glPushMatrix();//First node start
-	glTranslatef(0, -1, 0);
-	glRotatef(45, 0, 0, 1);
+	glRotatef(g1, 0, 0, 1);
+	glTranslatef(0, -0.75,  0);
 
 	glPushMatrix();
 	glTranslatef(0, 2, 0);
@@ -73,13 +75,24 @@ void Display()
 	glutWireSphere(0.5, 10, 10);
 	glPopMatrix();
 
+	glTranslatef(0, 3.25, 0);
+
 	glPushMatrix();//Second node start
+	glRotatef(g2, 0, 0, 1);
+	glTranslatef(0, -3.25, 0);
 
 	glPushMatrix();
 	glTranslatef(0, 4.5, 0);
 	glScalef(1, 2, 1);
 	glutWireCube(1);
 	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0, 5.75, 0);
+	glutWireSphere(0.5, 10, 10);
+	glPopMatrix();
+
+	glTranslatef(0, 5.75, 0);
 
 	glPopMatrix();//Second node end
 	glPopMatrix();//First node end
@@ -91,12 +104,20 @@ void TeclasEspeciais (int key, int x, int y)
 {
 	switch(key){
 		case GLUT_KEY_RIGHT:
+			if(!s)g0--;
+			if(s==1 && g1 > -45)g1--;
+			if(s==2 && g2 > -45)g2--;
 			break;
 		case GLUT_KEY_UP:
+			s = (s == 2)?0:s+1;
 			break;
 		case GLUT_KEY_LEFT:
+			if(!s)g0++;
+			if(s==1 && g1 < 45)g1++;
+			if(s==2 && g2 < 45)g2++;
 			break;
 		case GLUT_KEY_DOWN:
+			s = (!s)?2:s-1;
 			break;
 	}
 	glutPostRedisplay();
