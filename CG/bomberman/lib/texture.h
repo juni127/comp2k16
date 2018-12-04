4379357f0477470dbb4b857ac9af73eeb2aaf6cc
Reinaldo A. Junior <juni127@live.com>
2018-12-04 18:38:23 -0200

    Começo menu

--- CG/bomberman/lib/texture.h
+++ CG/bomberman/lib/texture.h
@@ -4,58 +4,53 @@
 #define MAX_IMAGE 32
 
 GLuint texid[MAX_IMAGE];
-int    image[MAX_IMAGE];
+ILuint image[MAX_IMAGE];
 
 
 // Inicia o DevIL
-void initDevil(){
+void inicia_textura(){
     if (ilGetInteger(IL_VERSION_NUM) < IL_VERSION){
         printf("wrong DevIL version \n");
         return -1;
     }
     ilInit(); 
+
+    ilGenImages(MAX_IMAGE, image); /* Generation of one image name */
+    glGenTextures(MAX_IMAGE, texid); /* Texture name generation */
 }
 
 /* Load an image using DevIL and return the devIL handle (-1 if failure) */
 int loadImage(char *filename)
 {
     ILboolean success; 
-    ILuint image; 
- 
-    ilGenImages(1, &image); /* Generation of one image name */
-     ilBindImage(image); /* Binding of image name */
-     success = ilLoadImage(filename); /* Loading of the image filename by DevIL */
- 
-    if (success) /* If no error occured: */
-    {
-        /* Convert every colour component into unsigned byte. If your image contains alpha channel you can replace IL_RGB with IL_RGBA */
-           success = ilConvertImage(IL_RGBA, IL_UNSIGNED_BYTE); 
- 
-        if (!success)
-           {
-                 return -1;
-           }
-    }
-    else
-        return -1;
+    success = ilLoadImage(filename); /* Loading of the image filename by DevIL */
+    success = ilConvertImage(IL_RGBA, IL_UNSIGNED_BYTE); 
  
     return image;
 }
 
-void openAndBind(char * path, int slot){
+void abrir_imagem(char * path, int slot){
     /* load the file picture with DevIL */
-    image[slot] = loadImage(path);
-    if (image[slot] == -1 )
-    {
-        //printf("Can't load picture file %s by DevIL \n", argv[1]);
-        return -1;
-    }
+    ilBindImage(image[slot]); /* Binding of image name */
+    loadImage(path);
  
     /* OpenGL texture binding of the image loaded by DevIL  */
-    glGenTextures(1, &(texid[slot])); /* Texture name generation */
     glBindTexture(GL_TEXTURE_2D, texid[slot]); /* Binding of texture name */
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); /* We will use linear interpolation for magnification filter */
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); /* We will use linear interpolation for minifying filter */
     glTexImage2D(GL_TEXTURE_2D, 0, ilGetInteger(IL_IMAGE_BPP), ilGetInteger(IL_IMAGE_WIDTH), ilGetInteger(IL_IMAGE_HEIGHT), 
     0, ilGetInteger(IL_IMAGE_FORMAT), GL_UNSIGNED_BYTE, ilGetData()); /* Texture specification */
+
+}
+
+void escolher_imagem(int slot){
+    /* load the file picture with DevIL */
+    ilBindImage(image[slot]); /* Binding of image name */
+ 
+    /* OpenGL texture binding of the image loaded by DevIL  */
+    glBindTexture(GL_TEXTURE_2D, texid[slot]); /* Binding of texture name */
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); /* We will use linear interpolation for magnification filter */
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); /* We will use linear interpolation for minifying filter */
+    glTexImage2D(GL_TEXTURE_2D, 0, ilGetInteger(IL_IMAGE_BPP), ilGetInteger(IL_IMAGE_WIDTH), ilGetInteger(IL_IMAGE_HEIGHT), 
+    0, ilGetInteger(IL_IMAGE_FORMAT), GL_UNSIGNED_BYTE, ilGetData()); /* Texture specification */    
 }
--- CG/bomberman/main.c
+++ CG/bomberman/main.c
@@ -25,22 +25,45 @@
 int width  = DEFAULT_WIDTH;
 int height = DEFAULT_HEIGHT;
 
+int ESTADO = 0;
+
+void menu_principal(){
+    /* Draw a quad */
+    abrir_imagem("img/fundo_titulo.png", 0);
+    glBegin(GL_QUADS);
+        glTexCoord2i(0, 0); glVertex2i(0,   0);
+        glTexCoord2i(0, 1); glVertex2i(0,   height+5);
+        glTexCoord2i(1, 1); glVertex2i(width+5, height+5);
+        glTexCoord2i(1, 0); glVertex2i(width+5, 0);
+    glEnd();
+
+    abrir_imagem("img/dirigivel_maior.png", 1);
+
+    glBegin(GL_QUADS);
+        glTexCoord2i(0, 0); glVertex2i(0,   0);
+        glTexCoord2i(0, 1); glVertex2i(0,   100);
+        glTexCoord2i(1, 1); glVertex2i(200, 100);
+        glTexCoord2i(1, 0); glVertex2i(200, 0);
+    glEnd();
+}
+
 /* Handler for window-repaint event. Called back when the window first appears and
    whenever the window needs to be re-painted. */
-void display() 
-{
+void display() {
+    /* Comum */
     // Clear color and depth buffers
-       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
-       glMatrixMode(GL_MODELVIEW);     // Operate on model-view matrix
- 
-    /* Draw a quad */
-       glBegin(GL_QUADS);
-           glTexCoord2i(0, 0); glVertex2i(0,   0);
-           glTexCoord2i(0, 1); glVertex2i(0,   height);
-           glTexCoord2i(1, 1); glVertex2i(width, height);
-           glTexCoord2i(1, 0); glVertex2i(width, 0);
-       glEnd();
- 
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+    glMatrixMode(GL_MODELVIEW);     // Operate on model-view matrix
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+
+    switch(ESTADO){
+        case 0:
+            //Menu principal
+            menu_principal();
+            break;
+    }
+
     glutSwapBuffers();
 }
 
@@ -54,30 +77,27 @@ void reshape(int width, int height) {
 /* Initialize OpenGL Graphics */
 void initGL(int w, int h) 
 {
-  glutInitDisplayMode(GLUT_DOUBLE); // Enable double buffered mode
-  glutInitWindowSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);   // Set the window's initial width & height
-  glutCreateWindow("Bomberman");      // Create window with the name of the executable
-  glutDisplayFunc(display);       // Register callback handler for window re-paint event
-  glutReshapeFunc(reshape);
-  glViewport(0, 0, w, h); // use a screen size of WIDTH x HEIGHT
-  glEnable(GL_TEXTURE_2D);     // Enable 2D texturing
+    glutInitDisplayMode(GLUT_DOUBLE); // Enable double buffered mode
+    glutInitWindowSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);   // Set the window's initial width & height
+    glutCreateWindow("Bomberman");      // Create window with the name of the executable
+    glutDisplayFunc(display);       // Register callback handler for window re-paint event
+    glutReshapeFunc(reshape);
+    glViewport(0, 0, w, h); // use a screen size of WIDTH x HEIGHT
+    glEnable(GL_TEXTURE_2D);     // Enable 2D texturing
  
-  glMatrixMode(GL_PROJECTION);     // Make a simple 2D projection on the entire window
-  glLoadIdentity();
-  glOrtho(0.0, w, h, 0.0, 0.0, 100.0);
+    glMatrixMode(GL_PROJECTION);     // Make a simple 2D projection on the entire window
+    glLoadIdentity();
+    glOrtho(0.0, w, h, 0.0, 0.0, 100.0);
  
-  glMatrixMode(GL_MODELVIEW);    // Set the matrix mode to object modeling
+    glMatrixMode(GL_MODELVIEW);    // Set the matrix mode to object modeling
  
-  glClearColor(0.0f, 0.0f, 0.0f, 0.0f); 
-  glClearDepth(0.0f);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the window
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); 
+    glClearDepth(0.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the window
 }
 
  
-int main(int argc, char **argv) 
-{
- 
- 
+int main(int argc, char **argv) {
     /* GLUT init */
     glutInit(&argc, argv);
     initGL(DEFAULT_WIDTH, DEFAULT_HEIGHT);
@@ -91,18 +111,16 @@ int main(int argc, char **argv)
 
 
     /* Initialization of DevIL */
-    initDevil();
-    openAndBind("img/main.png", 0);
-    //openAndBind("img/osama.png", 1);
+    inicia_textura();
 
     /* Main loop */
     glutMainLoop();
  
     /* Delete used resources and quit */
-     ilDeleteImages(1, &(image[0])); /* Because we have already copied image data into texture data we can release memory used by image. */
-     glDeleteTextures(1, &(texid[0]));
+    ilDeleteImages(1, &(image[0])); /* Because we have already copied image data into texture data we can release memory used by image. */
+    glDeleteTextures(1, &(texid[0]));
 
-     finaliza_audio();
+    finaliza_audio();
 
-     return 0;
+    return 0;
 }
