5df92fbc14c6eab290632d9ee7bbfee126b6a61e
Alunos <alunos@r07l12.cl.ibilce.unesp.br>
2018-12-05 17:35:21 -0200

    Tava tudo errado

--- CG/flappy/main.c
+++ CG/flappy/main.c
@@ -1,229 +1,129 @@
+
 #include<stdio.h>
-#include<stdlib.h> 
+#include<stdlib.h>
+
+// Tempo
 #include<time.h>
+
+// Glut
+#include <GL/glut.h>
+#include <GL/glu.h>
+#include <GL/gl.h>
 
 //Threads
 #include<pthread.h>
 
-//Glut
-#include<GL/gl.h>
-#include<GL/glu.h>
-#include<GL/glut.h>
+#define W 480
+#define H 640
 
-//DevIL - Imagens
-#include<IL/il.h>
-
-//mpg123 - Audio
-#include <ao/ao.h>
-#include <mpg123.h>
-
-//Organizacional
-#include"lib/texture.h"
-#include"lib/audio.h"
-#include"lib/animacao.h"
-
-#define DEFAULT_WIDTH  640
-#define DEFAULT_HEIGHT 480
-
-#define MAX_CANO 3
-
-int width  = DEFAULT_WIDTH;
-int height = DEFAULT_HEIGHT;
-
-pthread_t thread_sfx;
-
-vertex canos[MAX_CANO];
-float acel = 120, vel = 0, posicao = 240, temp = 0.05, vel_cano = -3;
-char gameover = 0, caindo = 0, gamemode = 0;
+//DeclaraÃ§Ã£o de variaveis
 
 
-void jogo2d(){
-    int x;
-    escolher_imagem(0);
-    desenhar_imagem(0, 0);
+//Protótipos das Funções
+void Display();
+void logicaJogo();
+void keyboard (unsigned char key, int x, int y);
+void TeclasEspeciais (int key, int x, int y);
 
-    for(x = 0; x < MAX_CANO; x++){
-        escolher_imagem(3);
-        desenhar_imagem(canos[x].x, canos[x].y+40);
-        escolher_imagem(4);
-        desenhar_imagem(canos[x].x, canos[x].y - 570);
+float s= 0, v = 0, a = -10, t = 0, tempo;
 
-        // ColisÃ£o com o cano
-        //printf("pos: %f  x: %f y: %f\n", posicao, canos[x].x, canos[x].y);
-        if(!caindo && 360 > canos[x].x && 300 < canos[x].x && (posicao < canos[x].y-90 || posicao > canos[x].y+20)){
-            pthread_cancel(&thread_sfx);
-            abrir_audio("sng/queda.mp3");
-            pthread_create(&thread_sfx, NULL, tocar_audio, NULL);
-            caindo = 1;
-            vel = 0;
-        }
-    }
+clock_t start, end;
 
-    // ColisÃ£o com o chÃ£o
-    if(posicao + 25 > 400){
-        gameover = 1;
-
-        //pthread_cancel(&thread_sfx);
-        //abrir_audio("sng/colisao.mp3");
-        //pthread_create(&thread_sfx, NULL, tocar_audio, NULL);
-    }
-
-    // ColisÃ£o com o teto
-    if(posicao < 0){
-        vel = 0;
-        posicao = 0;
-    }
-
-    escolher_imagem(2);
-    //glRotatef(90.0*vel/250.0, 0, 0, 1);
-    desenhar_imagem(340, posicao);
-
-    escolher_imagem(1);
-    desenhar_imagem(0, 400);
-
-}
-
-void jogo3d(){
-
-}
-
-void jogo1pessoa(){
-
-}
-
-/* Handler for window-repaint event. Called back when the window first appears and
-   whenever the window needs to be re-painted. */
-void display() {
-    /* Comum */
-    // Clear color and depth buffers
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
-    glMatrixMode(GL_MODELVIEW);     // Operate on model-view matrix
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+void logicaJogo(){
 
     end = clock();
-    tempo_passado += (end - start)*10000/CLOCKS_PER_SEC;
+    tempo = (end - start)*100.0/CLOCKS_PER_SEC;
     start = clock();
 
-    int x;
+    t += tempo;
 
-    switch(gamemode){
-        case 0:
-            jogo2d();
-            break;
-        case 1:
-            jogo3d();
-            break;
-        case 2:
-            jogo1pessoa();
-            break;
+    printf("t: %f\n", s);
+    if(t > 0.1){
+        s = s + v*t/10.0 + a*t*t/20.0;
+        v = v + a*t/10.0;
+
+        t = 0;
     }
-
-
-    if(tempo_passado > 1 && !gameover){
-        tempo_passado = 0;
-
-        posicao = posicao + vel * temp + acel * temp * temp/2;
-        vel = vel + acel * temp;
-
-        for(x = 0; x < MAX_CANO && !caindo; x++){
-            canos[x].x += vel_cano;
-            if(canos[x].x < -100){
-                canos[x].x = 640;
-                canos[x].y = rand()%300 + 100;
-            }
-        }
-    }
-
-
-    glutSwapBuffers();
 }
 
-void reshape(int width, int height) {
-    // we ignore the params and do:
-    glutReshapeWindow(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-}
-
-
-
-/* Initialize OpenGL Graphics */
-void initGL(int w, int h) 
+void Display()
 {
-    glutInitDisplayMode(GLUT_DOUBLE); // Enable double buffered mode
-    glutInitWindowSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);   // Set the window's initial width & height
-    glutCreateWindow("Flappy Bird");      // Create window with the name of the executable
-    glutDisplayFunc(display);       // Register callback handler for window re-paint event
-    glutReshapeFunc(reshape);
-    glViewport(0, 0, w, h); // use a screen size of WIDTH x HEIGHT
-    glEnable(GL_TEXTURE_2D);     // Enable 2D texturing
- 
-    glMatrixMode(GL_PROJECTION);     // Make a simple 2D projection on the entire window
-    glLoadIdentity();
-    glOrtho(0.0, w, h, 0.0, 0.0, 100.0);
- 
-    glMatrixMode(GL_MODELVIEW);    // Set the matrix mode to object modeling
- 
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); 
-    glClearDepth(0.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the window
+
+   glEnable(GL_DEPTH_TEST);
+
+   glEnable(GL_LINE_SMOOTH);
+   glEnable(GL_POLYGON_SMOOTH);
+
+
+   glEnable(GL_SMOOTH);
+   glEnable(GL_BLEND);
+
+   glClearColor(0.0, 0.0, 0.0, 0.0);
+
+
+   glMatrixMode(GL_PROJECTION);/*glMatrixMode()- define qual matriz será alterada. SEMPRE defina o tipo de apresentação 
+                              (Ortogonal ou Perspectiva) na matriz PROJECTION.*/
+   glLoadIdentity();//"Limpa" ou "transforma" a matriz em identidade, reduzindo possíveis erros.
+
+
+   gluPerspective(45,1,1,150);
+   //glOrtho(-8, 8, -8, 8, -150, 150);
+
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+   gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0); //Define a pos da câmera, para onde olha e qual eixo está na vertical.
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+   glColor3ub(255, 255, 255);
+
+
+   logicaJogo();
+
+   glPushMatrix();
+   glTranslatef(0, s, 0);
+   glutWireCube(1);
+   glPopMatrix();
+
+
+    glutSwapBuffers(); //Executa a Cena. SwapBuffers dá suporte para mais de um buffer, permitindo execução de animações sem cintilações.
+    glutPostRedisplay(); //Executa novamente
 }
 
-void processKeys(unsigned char key, int x, int y){
-    if(key == 32 && !caindo){
-        vel = -150;
-        
-        // Tocar som da batida das asas
-        //pthread_cancel(&thread_sfx);
-        //abrir_audio("sng/batida.mp3");
-        //pthread_create(&thread_sfx, NULL, tocar_audio, NULL);
-    }
+void TeclasEspeciais (int key, int x, int y)
+{
+	switch(key){
+		case GLUT_KEY_RIGHT:
+			break;
+		case GLUT_KEY_UP:
+			break;
+		case GLUT_KEY_LEFT:
+			break;
+		case GLUT_KEY_DOWN:
+			break;
+	}
+	glutPostRedisplay();
 }
 
- 
-int main(int argc, char **argv) {
-    /* GLUT init */
-    glutInit(&argc, argv);
-    initGL(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-    glutKeyboardFunc( processKeys );
+void TeclasNormais(unsigned char key, int x, int y){
+    if(key == 32)
+        v = 5;
+}
 
-    /* InicializaÃ§Ã£o do mpg123 */
-    inicia_audio();
 
-    int x = 0;
+int main(int argc,char **argv)
+{
+   glutInit(&argc, argv); // Initializes glut
 
-    /* Initialization of DevIL */
-    inicia_textura();
 
-    inicia_animacao();
+   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
-    srand (time(NULL));
+   glutInitWindowSize(W, H);
+   glutInitWindowPosition(100, 100);
+   glutCreateWindow("Flappy Bird");
+   glutDisplayFunc(Display);
+   glutSpecialFunc(TeclasEspeciais);
+   glutKeyboardFunc(TeclasNormais);
+   glutMainLoop();
 
-    for(x = 0; x < MAX_CANO; x++){
-        canos[x].x = 680 + x*(640/MAX_CANO);
-        canos[x].y = rand()%320 + 80;
-    }
-
-    abrir_imagem("img/fundo.png", 0);
-    abrir_imagem("img/chao.png", 1);
-    abrir_imagem("img/bird.png", 2);
-    abrir_imagem("img/cano.png", 3);
-    abrir_imagem("img/cano2.png", 4);
-
-/*
-    abrir_imagem("img/dirigivel_menor.png", 2);
-    desenhar_imagem(0, 225);
-
-    abrir_imagem("img/balao.png", 3);
-    desenhar_imagem(50, 25);
-*/
-    /* Main loop */
-    glutMainLoop();
- 
-    /* Delete used resources and quit */
-    ilDeleteImages(1, &(image[0])); /* Because we have already copied image data into texture data we can release memory used by image. */
-    glDeleteTextures(1, &(texid[0]));
-
-    finaliza_audio();
-
-    return 0;
+   return 0; 
 }
---
+++ CG/flappy_old/lib/texture.h
@@ -0,0 +1,65 @@
+//DevIL - Imagens
+#include<IL/il.h>
+
+#define MAX_IMAGE 32
+
+GLuint texid[MAX_IMAGE];
+ILuint image[MAX_IMAGE];
+
+
+// Inicia o DevIL
+void inicia_textura(){
+    if (ilGetInteger(IL_VERSION_NUM) < IL_VERSION){
+        printf("wrong DevIL version \n");
+        return -1;
+    }
+    ilInit(); 
+
+    ilGenImages(MAX_IMAGE, image); /* Generation of one image name */
+    glGenTextures(MAX_IMAGE, texid); /* Texture name generation */
+}
+
+/* Load an image using DevIL and return the devIL handle (-1 if failure) */
+int loadImage(char *filename)
+{
+    ILboolean success; 
+    success = ilLoadImage(filename); /* Loading of the image filename by DevIL */
+    success = ilConvertImage(IL_RGBA, IL_UNSIGNED_BYTE); 
+ 
+    return image;
+}
+
+void abrir_imagem(char * path, int slot){
+    /* load the file picture with DevIL */
+    ilBindImage(image[slot]); /* Binding of image name */
+    loadImage(path);
+ 
+    /* OpenGL texture binding of the image loaded by DevIL  */
+    glBindTexture(GL_TEXTURE_2D, texid[slot]); /* Binding of texture name */
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); /* We will use linear interpolation for magnification filter */
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); /* We will use linear interpolation for minifying filter */
+    glTexImage2D(GL_TEXTURE_2D, 0, ilGetInteger(IL_IMAGE_BPP), ilGetInteger(IL_IMAGE_WIDTH), ilGetInteger(IL_IMAGE_HEIGHT), 
+    0, ilGetInteger(IL_IMAGE_FORMAT), GL_UNSIGNED_BYTE, ilGetData()); /* Texture specification */
+
+}
+
+void escolher_imagem(int slot){
+    /* load the file picture with DevIL */
+    ilBindImage(image[slot]); /* Binding of image name */
+ 
+    /* OpenGL texture binding of the image loaded by DevIL  */
+    glBindTexture(GL_TEXTURE_2D, texid[slot]); /* Binding of texture name */
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); /* We will use linear interpolation for magnification filter */
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); /* We will use linear interpolation for minifying filter */
+    glTexImage2D(GL_TEXTURE_2D, 0, ilGetInteger(IL_IMAGE_BPP), ilGetInteger(IL_IMAGE_WIDTH), ilGetInteger(IL_IMAGE_HEIGHT), 
+    0, ilGetInteger(IL_IMAGE_FORMAT), GL_UNSIGNED_BYTE, ilGetData()); /* Texture specification */    
+}
+
+void desenhar_imagem(float x, float y){
+    glBegin(GL_QUADS);
+        glTexCoord2i(0, 0); glVertex2f(x,   y);
+        glTexCoord2i(0, 1); glVertex2f(x,   y + ilGetInteger(IL_IMAGE_HEIGHT));
+        glTexCoord2i(1, 1); glVertex2f(x + ilGetInteger(IL_IMAGE_WIDTH), y + ilGetInteger(IL_IMAGE_HEIGHT));
+        glTexCoord2i(1, 0); glVertex2f(x + ilGetInteger(IL_IMAGE_WIDTH), y);
+    glEnd();
+}
